c9a1089a12df40d3fd8fbe474ce43fe1a1e43c35
Andreas Rammhold <andreas@rammhold.de>
2020-01-07 02:19:05 +0100

    pin nixpkgs version

---
+++ nixpkgs.nix
@@ -0,0 +1,1 @@
+import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/cca0c894a13ec398de8b046174239513197f5c74.tar.gz") {}
--- shell.nix
+++ shell.nix
@@ -1,11 +1,11 @@
+{ pkgs ? import ./nixpkgs.nix }:
 let
- nixpkgs = import <nixpkgs> {};
- crate2nix = nixpkgs.callPackage (nixpkgs.fetchFromGitHub {
+ crate2nix = pkgs.callPackage (pkgs.fetchFromGitHub {
    owner = "kolloch";
    repo = "crate2nix";
    rev = "5b19c1b14e1b0e5522c3e44e300d0b332dc939e7";
    sha256 = "0j5cnzxbikv9zxw50vcbzndg3kzacjk7a05m3sph5jk7yrxsk7s5";
  }) {};
-in nixpkgs.mkShell {
+in pkgs.mkShell {
   buildInputs = [ crate2nix ];
 }
