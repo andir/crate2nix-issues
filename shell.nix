94c036fbe0d870e5d73c2cfea4b10a676f6e0334
Andreas Rammhold <andreas@rammhold.de>
2020-01-07 02:13:16 +0100

    update to current crate2nix version

--- shell.nix
+++ shell.nix
@@ -1,10 +1,10 @@
 let
-  nixpkgs = import <nixpkgs> {};
+ nixpkgs = import <nixpkgs> {};
  crate2nix = nixpkgs.callPackage (nixpkgs.fetchFromGitHub {
    owner = "kolloch";
    repo = "crate2nix";
-   rev = "f3bac5fcc0d88dda9f060e885028f73a42857a33";
-   sha256 = "02cc3pg1y9a73w3knb2x6zrrivq39kb66pfz4ivsi10g6mkyczgh";
+   rev = "5b19c1b14e1b0e5522c3e44e300d0b332dc939e7";
+   sha256 = "0j5cnzxbikv9zxw50vcbzndg3kzacjk7a05m3sph5jk7yrxsk7s5";
  }) {};
 in nixpkgs.mkShell {
   buildInputs = [ crate2nix ];
